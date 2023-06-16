4a1ebae3cf3735e06dd0cd549dee18deab5a0c56
Thomaz.Apenas <73339593+Thomaz-py@users.noreply.github.com>
2023-06-16 18:58:58 +0000

    thmzzzzz

---
+++ javasc/script.js
@@ -0,0 +1,35 @@
+function boasvindas() {
+    
+
+let nome;
+let sobrenome;
+
+nome = prompt("Digite o Seu Nome","Digite Aqui")
+sobrenome = prompt("Digite o Seu Sobreome","Digite Aqui")
+
+alert("Olá "+nome+" "+sobrenome)
+}
+function somar() {
+    let n1 = prompt("Digite um nmero","Digite Aqui")
+    let n2 = prompt("Digite um nmero","Digite Aqui")
+    let n3 = parseInt(n1)+parseInt(n2)
+    alert("A somas de ambos é ="+n3)
+}
+function subitrair() {
+    let n1 = prompt("Digite um nmero","Digite Aqui")
+    let n2 = prompt("Digite um nmero","Digite Aqui")
+    let n3 = parseInt(n1)-parseInt(n2)
+    alert("A subitração de ambos é ="+n3)
+}
+function mutiplicar() {
+    let n1 = prompt("Digite um nmero","Digite Aqui")
+    let n2 = prompt("Digite um nmero","Digite Aqui")
+    let n3 = parseInt(n1)*parseInt(n2)
+    alert("A mutiplicação de ambos é ="+n3)
+}
+function dividir() {
+    let n1 = prompt("Digite um nmero","Digite Aqui")
+    let n2 = prompt("Digite um nmero","Digite Aqui")
+    let n3 = parseInt(n1)/parseInt(n2)
+    alert("A divisão de ambos é ="+n3)
+}
